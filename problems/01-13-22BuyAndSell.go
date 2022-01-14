bdd20417142e4001e6d954d96710b162e93166f6
spencer allen <allensg@users.noreply.github.com>
2022-01-13 20:13:02 -0500

    solving buyandsell

--- problems/01-13-22BuyAndSell.go
+++ problems/01-13-22BuyAndSell.go
@@ -1,13 +1,31 @@
 package problems
 
+import "fmt"
+
 // You are given an array. Each element represents the price of a stock on that particular day.
 // Calculate and return the maximum profit you can make from buying and selling that stock only once
 
 // Input: [9, 11, 8, 5, 7, 10]
 // Output: 10-5 = 5
-func (h *Handler) BuyAndSell(input []int) (returnString string, answer int) {
+func (h *Handler) BuyAndSell(input []int) (returnString string, maxProfit int) {
 
-	returnString = "Pythag tripples for set: "
+	// traversing the array backwards keeps it at O(n)
+	currentHighest := 0
+	maxProfit = 0
+	for i := len(input) - 1; i >= 0; i-- {
+		currentElement := input[i]
+		if currentElement > currentHighest {
+			currentHighest = currentElement
+		} else {
+			currentProfit := currentHighest - currentElement
+			if currentProfit > maxProfit {
+				maxProfit = currentProfit
+			}
+		}
+	}
 
-	return returnString, answer
+	strArr := h.IntArrToString(input)
+	returnString = fmt.Sprintf("The Max Profit that can be gained on day %s is %d", strArr, maxProfit)
+
+	return returnString, maxProfit
 }
---
+++ tests/BuyAndSell_test.go
@@ -0,0 +1,37 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/allensg/codingProblems/problems"
+)
+
+func TestBuyAndSell(t *testing.T) {
+	// Initialize handler
+	problems := &problems.Handler{}
+	var tests = []struct {
+		input  []int
+		answer int
+		desc   string
+	}{
+		{[]int{9, 11, 8, 5, 7, 10}, 5, "Success Case"},
+		{[]int{8, 5, 4, 3, 2, 1}, 0, "No profit available"},
+		{[]int{1, 2, 3, 4, 5, 44}, 43, "All profit"},
+	}
+
+	for _, testValues := range tests {
+
+		inputArrStr := problems.IntArrToString(testValues.input)
+		testname := fmt.Sprintf("%s", testValues.desc)
+		t.Run(testname, func(t *testing.T) {
+			returnString, outAnswer := problems.BuyAndSell(testValues.input)
+			fmt.Println(returnString)
+			if outAnswer != testValues.answer {
+				t.Errorf("input: %s | want %d | got %d", inputArrStr, testValues.answer, outAnswer)
+			} else {
+				t.Logf("input: %s | want %d | got %d", inputArrStr, testValues.answer, outAnswer)
+			}
+		})
+	}
+}

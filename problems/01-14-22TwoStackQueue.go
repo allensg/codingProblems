bcf347b15c77c3cf6f8e31e3cc1b12860039363c
spencer allen <allensg@users.noreply.github.com>
2022-01-14 12:13:47 -0500

    setup for two stack queue

---
+++ problems/01-14-22TwoStackQueue.go
@@ -0,0 +1,26 @@
+package problems
+
+// Implement a queue class using two stacks. A queue is a data structure that supports
+// the FIFO protocol (First in = first out). Your class should support the enqueue and dequeue methods like a standard queue.
+// implement constructor, enqueue, dequeue
+
+type Queue struct {
+	stuff string
+}
+
+func (h *Handler) TwoStackQueue(input []int) (returnString string, maxProfit int) {
+
+	return returnString, maxProfit
+}
+
+func (q *Queue) Build() {
+
+}
+
+func (q *Queue) Enqueue(val string) {
+
+}
+
+func (q *Queue) Dequeue(val string) {
+
+}

92adfddb0e5d568c548d4d2476ff238a4136ef00
spencer allen <allensg@users.noreply.github.com>
2022-01-16 11:34:02 -0500

    sortlinkedlists setup

---
+++ problems/01-16-22SortLinkedLists.go
@@ -0,0 +1,17 @@
+package problems
+
+// You are given an array of k sorted singly linked lists. Merge the linked lists into a single sorted linked list and return it.
+// Your solution should run in linear time.
+
+// a = Node(1, Node(3, Node(5)))
+// b = Node(2, Node(4, Node(6)))
+// print merge([a, b])
+// # 123456
+type LinkedList struct {
+	thing int
+}
+
+func (h *Handler) SortLinkedLists(input []int) (returnString string, subArray []LinkedList) {
+
+	return returnString, subArray
+}

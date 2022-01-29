90fb9fd7bbb8333f6d2f3751da796e66ceeb81f6
spencer allen <allensg@users.noreply.github.com>
2022-01-29 14:05:02 -0500

    adding word ordering

---
+++ problems/01-29-22WordOrdering.go
@@ -0,0 +1,11 @@
+package problems
+
+// Given a list of words, and an arbitrary alphabetical order,
+// verify that the words are in order of the alphabetical order.
+
+// input: words = ["abcd", "efgh"], order="zyxwvutsrqponmlkjihgfedcba"
+// output: False
+func (h *Handler) WordOrdering(input []string, order string) (returnString string, answer bool) {
+
+	return returnString, answer
+}

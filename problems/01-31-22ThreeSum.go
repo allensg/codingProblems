9ccc87873bb78b549c0c50c40ded92e0c78dbd93
spencer allen <allensg@users.noreply.github.com>
2022-01-30 20:45:50 -0500

    adding 3sum

---
+++ problems/01-31-22ThreeSum.go
@@ -0,0 +1,11 @@
+package problems
+
+// Given an array, nums, of n integers, find all unique triplets (three numbers, a, b, & c) in nums such that a + b + c = 0.
+//  Note that there may not be any triplets that sum to zero in nums, and that the triplets must not be duplicates.
+
+// Input: nums = [0, -1, 2, -3, 1]
+// output: Output: [0, -1, 1], [2, -3, 1]
+func (h *Handler) ThreeSum(input []int) (returnString string, answer [][]int) {
+
+	return returnString, answer
+}

555026a4f517d9f25a42940acc4c678da6ec1f04
spencer allen <allensg@users.noreply.github.com>
2022-02-02 11:25:43 -0500

    catching up on adding more problems

---
+++ problems/02-02-22CountInvalidParens.go
@@ -0,0 +1,15 @@
+package problems
+
+// Given a 2-dimensional grid consisting of 1's (land blocks) and 0's (water blocks),
+// count the number of islands present in the grid. The definition of an island is as follows:
+// 1.) Must be surrounded by water blocks.
+// 2.) Consists of land blocks (1's) connected to adjacent land blocks (either vertically or horizontally).
+// Assume all edges outside of the grid are water.
+
+// Input: ()())()
+// Output: 1
+
+func (h *Handler) CountInvalidParens(input string) (returnString string, answer int) {
+
+	return returnString, answer
+}

78796bf02b0ed456b992ba152c8bc215e3f01248
spencer allen <allensg@users.noreply.github.com>
2022-02-07 17:12:26 -0500

    Adding findRanges problem

---
+++ problems/02-07-22FindRanges.go
@@ -0,0 +1,11 @@
+package problems
+
+// Given a sorted list of numbers, return a list of strings that represent all of the consecutive numbers.
+
+// Input: [0, 1, 2, 5, 7, 8, 9, 9, 10, 11, 15]
+// Output: ['0->2', '5->5', '7->11', '15->15']
+
+func (h *Handler) FindRanges(input string) (returnString string, answer string) {
+
+	return returnString, answer
+}

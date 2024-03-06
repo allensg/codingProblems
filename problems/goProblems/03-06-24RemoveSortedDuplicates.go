1aa2c1f85687f948faada2ab00aa0760838e4988
spencer allen <allensg@users.noreply.github.com>
2024-03-06 15:57:11 -0800

    updating remove sorted duplicates with part 2

--- problems/goProblems/03-06-24RemoveSortedDuplicates.go
+++ problems/goProblems/03-06-24RemoveSortedDuplicates.go
@@ -12,7 +12,7 @@ import (
 // Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
 // The remaining elements of nums are not important as well as the size of nums.
 // Return k.
-func (h *Handler) removeSortedDuplicates(nums []int, val int) string {
+func (h *Handler) removeSortedDuplicatesPart1(nums []int, val int) string {
 	collisionMap := make(map[int]bool)
 	for index, value := range nums {
 		hit := collisionMap[value]
@@ -22,6 +22,35 @@ func (h *Handler) removeSortedDuplicates(nums []int, val int) string {
 			nums[index] = 101
 		} else {
 			collisionMap[value] = true
+		}
+	}
+
+	slices.Sort(nums)
+	return string(len(collisionMap))
+}
+
+// the next problem in the sequence is an expansion on the first. Scoring the second solution and qeustion here.
+
+// Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears AT MOST TWICE.
+// The relative order of the elements should be kept the same.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first
+// part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+func (h *Handler) removeSortedDuplicatesPart2(nums []int, val int) string {
+	failcaseCount := 0
+	collisionMap := make(map[int]int)
+	for index, value := range nums {
+		hit := collisionMap[value]
+		// if we've already come across this number it is a duplicate
+		if hit > 1 {
+			failcaseCount++
+			// go past max value for sorting later and add to count
+			nums[index] = 101
+		} else {
+			collisionMap[value] = hit + 1
 		}
 	}
 

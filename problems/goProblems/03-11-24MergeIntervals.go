2c2cc6753f8891d6d3a1ce32834277d076b6f06a
spencer allen <allensg@users.noreply.github.com>
2024-03-13 03:39:09 -0700

    completed merge intervals

--- problems/goProblems/03-11-24MergeIntervals.go
+++ problems/goProblems/03-11-24MergeIntervals.go
@@ -1,5 +1,7 @@
 package goProblems
 
+import "sort"
+
 // Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
 // and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
@@ -14,16 +16,25 @@ func (h *Handler) mergeInterval(intervals [][]int) (string, [][]int) {
 		return "", toReturn
 	}
 
-	
-	// i think the solution for this is to check the ranges and merge. 
-	// so if the end of range 1 is greater than the start of range 2, merge them
-	// then repeat for each element
-	rangeStart, rangeEnd := intervals[0][0], intervals[0][1]
-	for i := 0; len(intervals) > i; i++ {
-		// leftStart, leftEnd := intervals[]
-		rightStart, rightEnd := intervals[]		
+	// Sort the slice (array) of intervals intervals
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
 
+	toReturn = append(toReturn, intervals[0])
+
+	// We iterate on every items (except the first one) of the intervals slice
+	// and we compare it with the last item of the mergedIntervals slice
+	for _, interval := range intervals[1:] {
+		mergedEnd := toReturn[len(toReturn)-1]
+
+		if interval[0] > mergedEnd[1] {
+			// If the interval "completely overlap" append interval, overwriting mergedEnd on the next iteration
+			toReturn = append(toReturn, interval)
+		} else if interval[1] > mergedEnd[1] {
+			// Else if interval "overlap" (but not completely) mergeEnd: change the 1 value of interval for the 1 value of top
+			mergedEnd[1] = interval[1]
+		}
 	}
-
 	return "", toReturn
 }
